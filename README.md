37f10f3eb1af9e8c08b9870c33172dd996b34243
kenjis <kenji.uui@gmail.com>
2015-06-16 20:57:31 +0900

    Initial commit

---
+++ README.md
@@ -0,0 +1,26 @@
+# CodeIgniter3 Filename Checker
+
+This package checks CodeIgniter 3.0 class filename.
+
+## Installation
+
+Install `application/controller/Check.php` in your CodeIgniter 3.0 project.
+
+## Usage
+
+Access to `http://your-server/check/filename`.
+
+Or you can run via CLI:
+	
+~~~
+$ php index.php check filename
+~~~
+
+## Related Projects for CodeIgniter 3.0
+
+* [CodeIgniter Composer Installer](https://github.com/kenjis/codeigniter-composer-installer)
+* [Cli for CodeIgniter 3.0](https://github.com/kenjis/codeigniter-cli)
+* [CI PHPUnit Test](https://github.com/kenjis/ci-phpunit-test)
+* [CodeIgniter Simple and Secure Twig](https://github.com/kenjis/codeigniter-ss-twig)
+* [CodeIgniter Doctrine](https://github.com/kenjis/codeigniter-doctrine)
+* [CodeIgniter Deployer](https://github.com/kenjis/codeigniter-deployer)
---
+++ application/controllers/Check.php
@@ -0,0 +1,91 @@
+<?php
+
+class Check extends CI_Controller
+{
+	private $dir;
+	private $output_ = array();
+
+	public function __construct()
+	{
+		parent::__construct();
+		
+		$this->dir = array(
+			APPPATH . 'controllers',
+			APPPATH . 'libraries',
+			APPPATH . 'models',
+		);
+	}
+
+	public function filename()
+	{
+		foreach ($this->dir as $dir)
+		{
+			$this->recursiveCheckFilename($dir);
+		}
+		
+		$this->display();
+	}
+
+	private function recursiveCheckFilename($dir)
+	{
+		$iterator = new RecursiveRegexIterator(
+			new RecursiveDirectoryIterator($dir),
+			'/\A.+\.php\z/i',
+			RecursiveRegexIterator::GET_MATCH
+		);
+
+		foreach(new RecursiveIteratorIterator($iterator) as $file)
+		{
+			$filename = preg_replace(
+				'/'.preg_quote(APPPATH, '/').'/', 'APPPATH/', $file[0]
+			);
+			
+			if (! $this->checkFilename($filename))
+			{
+				$this->output('Error: ' . $filename);
+			}
+			else
+			{
+				$this->output('Okay: ' . $filename);
+			}
+		}
+	}
+
+	private function output($line)
+	{
+		$this->output_[] = $line . PHP_EOL;
+	}
+
+	private function display()
+	{
+		sort($this->output_);
+		
+		if (! is_cli())
+		{
+			echo "<pre>\n";
+		}
+		
+		foreach ($this->output_ as $line)
+		{
+			echo $line;
+		}
+		
+		if (! is_cli())
+		{
+			echo "</pre>\n";
+		}
+	}
+
+	private function checkFilename($filepath)
+	{
+		$filename = basename($filepath);
+		if (ucfirst($filename) !== $filename)
+		{
+			return FALSE;
+		}
+		else
+		{
+			return TRUE;
+		}
+	}
+}
